09a121655f4cd1ed9a7eda7867b603480df2fbdf
humanitarianist <seyitbilal_199@hotmail.com>
2015-10-21 01:10:28 +0300

    asama-03

--- odev03/caesar_thread.py
+++ odev03/caesar_thread.py
@@ -1,12 +1,4 @@
-
 import threading
-
-s, n, l = 1, 6, 10
-k, m = 0, 1
-newtextline = []
-textoneline = []
-lenline = []
-
 
 class MyThread (threading.Thread):
     def __init__(self, threadID):
@@ -14,48 +6,71 @@ class MyThread (threading.Thread):
         self.threadID = threadID
 
     def run(self):
-        process_data()
+        getline()
 
-def process_data():
-    exitFlag = False
-    while not exitFlag:
-        lock_key.acquire()
-        k = (k + 1) % 6
-        lock_key.release()
-        textoneline[k] = rfl.readline()
-        lenline[k] = len(textoneline[k])
-        textoneline[k].upper()
-        if lenline[k] is not 0:
-            for i in range(0, lenline[k]):
-                for j in range(0, 26):
-                    if textoneline[i] is alphabet[j]:
-                        newtextline[k][i] = alphabet[(j + s) % 26]
-                        break
-        else:
-            exitFlag = True
+def getline():
+    global orderkey
+    lock_key.acquire()
+    textoneline = rfl.readline()
+    print orderkey
+    orderkey+=1
 
-        while k is not m:
-            pass
+    encode(textoneline,orderkey-1)
+    lock_key.release()
+def encode(line, order):
+    newline = ""
+    if len(line) is not 0:
+        bigline = line.upper()
+        for i in bigline:
+            if i in "0123456789.,'^+%&/()=?_-*\";:><|\}][{$#\n\t ":
+                newline += i
+                continue
+            for j in alphabet:
+                if i is j:
+                    newline += alphabet[alphabet.index(j)]
+                    break
+        putline(newline,order)
+        getline()
 
-        wfl.write(newtextline[k])
-        m = m + 1
+def putline(codeline,order):
+    global orderline
+    lock_key.acquire()
+    workfinished = False
+    if order >= len(orderline):
+        for i in (len(orderline), order):
+            orderline.append(codeline)
+        print orderline
+    else:
+        del orderline[order]
+        orderline.insert(order, codeline)
+        print orderline
+    lock_key.release()
+    workfinished = True
 
+s, n = 1, 6
 alphabet  = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 lock_key = threading.Lock()
 threads = []
+orderkey = 0
+orderline = []
+workfinished = False
 
 rfl = open("metin.txt", "r")
-wfl = open("crypted_" + repr(s) + "_" + repr(n) + "_" + repr(l) + ".txt", 'w')
+wfl = open("crypted_" + repr(s) + "_" + repr(n) + "_" + "line" + ".txt", 'w')
 
 # Create new threads
 for tID in range(0, n):
     thread = MyThread(tID)
     thread.start()
+    threads = []
     threads.append(thread)
+
 
 # Wait for all threads to complete
 for t in threads:
     t.join()
 
-rfl.close()
+for i in orderline:
+    wfl.write(i)
+
 wfl.close()
