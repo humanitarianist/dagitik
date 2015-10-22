3477dde97f7aec3cc444dbb680a32c94d6dd3a8c
humanitarianist <seyitbilal_199@hotmail.com>
2015-10-22 20:31:59 +0300

    fork icin

--- odev03/caesar_fork.py
+++ odev03/caesar_fork.py
@@ -1,23 +1,10 @@
-import threading
-import Queue
-
-
-class MyThread (threading.Thread):
-    def __init__(self, threadID):
-        lock_key1.acquire()
-        threading.Thread.__init__(self)
-        self.threadID = threadID
-        lock_key1.release()
-
-    def run(self):
-        getline()
-
+from multiprocessing import Lock, Process, Queue
 
 def getline():
     lock_key1.acquire()
     textoneline = rfl.readline()
-    threadqueue.put(thread.threadID)
-    encode(thread.threadID, textoneline, len(textoneline))
+    processqueue.put(p.pid)
+    encode(Id, textoneline, len(textoneline))
     lock_key1.release()
 
 
@@ -34,10 +21,9 @@ def encode(Id, line, lenght):
                 break
     putline(Id, newline, lenght)
 
-
 def putline(Id, codeline, lenght):
     global a
-    dizi.append(threadqueue.get())
+    dizi.append(processqueue.get())
     lock_key2.acquire()
     if Id is dizi[0]:
         wfl.write(codeline)
@@ -50,38 +36,27 @@ def putline(Id, codeline, lenght):
 
 s, n = 1, 6
 alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-threadqueue = Queue.Queue()
-lock_key1, lock_key2 = threading.Lock(), threading.Lock()
-threads = []
+processqueue = Queue()
+lock_key1, lock_key2 = Lock(), Lock()
+process = []
 dizi = []
 a = 1
+Id = 0
 
 rfl = open("metin.txt", "r")
 wfl = open("crypted_" + repr(s) + "_" + repr(n) + "_" + "line" + ".txt", 'w')
 
-# Create new threads
-for tID in range(0, n):
-    thread = MyThread(tID)
-    thread.start()
-    threads.append(thread)
-
-
-# Wait for all threads to complete
-for t in threads:
-    t.join()
-
-
 while a is not 0:
-    # Create new threads
+    # Create new process
     for tID in range(0, n):
-        thread = MyThread(tID)
-        thread.start()
-        threads.append(thread)
+        p = Process(target = getline)
+        p.start()
+        Id+=1
 
 
-# Wait for all threads to complete
-    for t in threads:
-        t.join()
+    for tID in range(0, n):
+        p.join()
+
 
 wfl.close()
 rfl.close()
