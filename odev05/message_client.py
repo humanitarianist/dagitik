4ef2c567a1691b074437806b672ee7998d1b4681
humanitarianist <seyitbilal_199@hotmail.com>
2015-12-25 02:06:12 +0200

    odev05 v-5

--- odev05/message_client.py
+++ odev05/message_client.py
@@ -1,22 +1,77 @@
 #!/usr/bin/env python
 import socket
 import threading
+import sys
+from PyQt4.QtCore import *
+from PyQt4.QtGui import *
+import Queue
+import time
 
 class readThread (threading.Thread):
 
-    def __init__(self, serverSocket):
+    def __init__(self, serverSocket, app):
         threading.Thread.__init__(self)
         self.serverSocket = serverSocket
+        self.app = app
+
+    def incomingParser(self, data):
+
+        if len(data) == 0:
+            return
+
+        if len(data) > 3 and not data[3] == " ":
+            print data + "a1"
+            response = "ERR"
+            self.serverSocket.send(response)
+            return
+
+        elif data[0:3] == "ERL":
+            self.app.cprint("Server: Giris yapilmadi.")
+
+        elif data[0:3] == "ERR":
+            print "a2"
+            self.app.cprint("Server: Eksik veya hatali komut")
+
+        elif data[0:3] == "HEL":
+            self.app.cprint("Server: " + data[4:] + " oturuma katildiniz")
+
+        elif data[0:3] == "REJ":
+            self.app.cprint("Server: " + data[4:] + " kullanicisi oturumda mevcut. Baska bir nickname ile giris yapmalisiniz.")
+
+        elif data[0:3] == "LSA":
+            splitted = data[4:].split(":")
+            msg = "Server: -Kayitli kullanicilar- "
+            for i in splitted:
+                msg += i + ","
+            msg = msg[:-1]
+
+        elif data[0:3] == "MNO":
+            self.app.cprint("Server: Boyle bir nickname'e sahip kulllanici bulunmamaktadir.")
+
+        if " " in data[4:]:
+            indexRouter = data[4:].index(" ")
+            fromWho = data[4:indexRouter]
+            message = data[indexRouter+1:]
+
+            if ":" in fromWho:
+                indexRouter = data[4:].index(":")
+                fromWho = data[4:indexRouter]
+
+        if data[0:3] == "SAY":
+            self.app.cprint("<" + fromWho + ">" + message)
+
+
+        if data[0:3] == "MSG":
+            self.app.cprint("''" + fromWho + "''" + message)
+
 
     def run(self):
         while True:
             fromServer = self.serverSocket.recv(1024)
-            lastinst = "BYE"
-            if(lastinst in fromServer):
-                print fromServer
+            self.incomingParser(fromServer)
+            print fromServer
+            if fromServer[0:3] == "BYE":
                 break
-            else:
-                print fromServer
 
 
 class writeThread (threading.Thread):
@@ -27,25 +82,124 @@ class writeThread (threading.Thread):
 
     def run(self):
         while True:
-            fromClient = raw_input()
-            lastInst = "QUI"
+
+            fromClient = sendQueue.get()
+
             self.serverSocket.send(fromClient)
-            if fromClient[0:3] == lastInst:
+            if fromClient[0:3] == "QUI":
                 break
 
+
+class clientDialog(QDialog):
+    ''' An example application for PyQt. Instantiate and call the run method to run. '''
+
+    def __init__(self, threadQueue):
+
+        self.threadQueue = threadQueue
+
+        # create a Qt application --- every PyQt app needs one
+        self.qt_app = QApplication(sys.argv)
+
+        # Call the parent constructor on the current object
+        QDialog.__init__(self, None)
+
+        # Set up the window
+        self.setWindowTitle('IRC Client')
+        self.setMinimumSize(500, 200)
+
+        # Add a vertical layout
+        self.vbox = QVBoxLayout()
+
+        # The sender textbox
+        self.sender = QLineEdit("", self)
+
+        # The channel region
+        self.channel = QTextBrowser()
+
+        # The send button
+        self.send_button = QPushButton('&Send')
+
+        # Connect the Go button to its callback
+        self.send_button.clicked.connect(self.outgoing_parser)
+
+        # Add the controls to the vertical layout
+        self.vbox.addWidget(self.channel)
+        self.vbox.addWidget(self.sender)
+        self.vbox.addWidget(self.send_button)
+
+        # A very stretchy spacer to force the button to the bottom
+        # self.vbox.addStretch(100)
+        # Use the vertical layout for the current window
+        self.setLayout(self.vbox)
+
+    def cprint(self, data):
+        self.channel.append(data)
+
+    def outgoing_parser(self):
+        # textbox'tan qstring cekiyoruz
+        data = self.sender.text()
+
+        # qstring'i string'e ceviriyoruz
+        data = str(data)
+
+        if len(data) == 0:
+            return
+
+        self.cprint("Local: " + data)
+
+        if data[0] == "/":
+
+            if " " in data:
+                indexRouter = data.index(" ")
+                command = data[1:indexRouter]
+                arguments = data[indexRouter+1:]
+            else:
+                command = data[1:]
+                arguments = None
+
+            if command == "nick":
+                self.threadQueue.put("USR " + arguments)
+
+            elif command == "list":
+                self.threadQueue.put("LSQ")
+
+            elif command == "msg":
+                indexRouter = arguments.index(" ")
+                toNickname = arguments[1:indexRouter]
+                message = arguments[indexRouter+1:]
+                self.threadQueue.put("MSG " + toNickname + ":" + message)
+
+            elif command == "quit":
+                self.threadQueue.put("QUI")
+
+            else:
+                self.cprint("Local: Command Error.")
+
+        else:
+            self.threadQueue.put("SAY " + data)
+
+        self.sender.clear()
+
+    def run(self):
+        ''' Run the app and show the main form. '''
+        self.show()
+        self.qt_app.exec_()
 
 serverSocket = socket.socket()
 
 host = socket.gethostname()
-port = 12346
+port = 12345
 
 serverSocket.connect((host, port))
 
-rThread = readThread(serverSocket)
+sendQueue = Queue.Queue()
+app = clientDialog(sendQueue)
+
+rThread = readThread(serverSocket, app)
 wThread = writeThread(serverSocket)
 
 rThread.start()
 wThread.start()
-
+app.run()
 rThread.join()
 wThread.join()
--- odev05/message_server.py
+++ odev05/message_server.py
@@ -6,11 +6,160 @@ import threading
 import Queue
 import time
 
+
 class readThread (threading.Thread):
 
     def __init__(self, threadName, nickname, threadID, clientSocket, clientAddr, fihrist, threadQueue, logQueue):
         threading.Thread.__init__(self)
-        self.threadName += "read" + threadName
+        self.threadName = "read" + threadName
+        self.nickname = nickname
+        self.threadID = threadID
+        self.clientSocket = clientSocket
+        self.clientAddr = clientAddr
+        self.fihrist = fihrist
+        self.threadQueue = threadQueue
+        self.logQueue = logQueue
+
+    def Parser(self, data):
+        data = data.strip()
+
+        # henuz giris yapmamis ise ve giris yapmadan islem yapmak isterse "ERL" dondur
+        if not self.nickname and not data[0:3] == "USR":
+            response = "ERL"
+            return [response, 0]
+
+        # Daha onceden login olmus ise ve login halinde tekrar login olmak istiyorsa "ERL" dondur
+        elif self.nickname and data[0:3] == "USR":
+            response = "ERL"
+            return [response, 0]
+
+        # Kullanici giris yapmak istiyorsa
+        elif data[0:3] == "USR" :
+            self.nickname = data[4:]
+
+            # Nickname vermeden giris yapmak isterse
+            if not self.nickname:
+                response = "ERL"
+                return [response, 0]
+
+            # Nickname kullanima uygunsa
+            lock1.acquire()
+            if not self.fihrist.has_key(self.nickname):
+                response = "HEL " + self.nickname
+                self.fihrist[self.nickname] = self.threadQueue
+                lock2.acquire()
+                self.logQueue.put(self.nickname + " has joined.")
+                lock2.release()
+                lock1.release()
+                return [response, 0]
+
+            # Nickname kullanima uygun degilse
+            else:
+                # kullanici reddedilecek
+                response = "REJ " + self.nickname
+                self.nickname = None
+                lock1.release()
+                return [response, 0]
+
+        # Kullanici cikis yapmak istiyorsa
+        elif data[0:3] == "QUI":
+            response = "BYE " + self.nickname
+
+            lock1.acquire()
+            for i in fihrist.keys():
+                self.fihrist[i].put(self.nickname + " kullanicisi oturumu sonlandirdi.")
+            lock1.release()
+
+            # fihristten sil
+            lock1.acquire()
+            del fihrist [self.nickname]
+            lock1.release()
+            # baglantiyi sil
+            return [response, 1]
+
+        # Kullanici diger kullanicilari listelemek istiyorsa
+        elif data[0:3] == "LSQ":
+            response = "LSA "
+            lock1.acquire()
+            for i in fihrist.keys():
+                response += i + ": "
+            lock1.release()
+            response = response.strip(": ")
+            return [response, 0]
+
+        # Baglanti kontrol komutu
+        elif data[0:3] == "TIC":
+            response = "TOC"
+            return [response, 0]
+
+        # Kullanici genel mesaj yollamak isterse
+        elif data[0:3] == "SAY":
+            queue_message = (self.nickname, data[4:])
+            lock1.acquire()
+            for i in fihrist.keys():
+                self.fihrist[i].put(queue_message)
+            lock1.release()
+            response = ""
+            return [response, 0]
+
+        # Kullanici ozel mesaj yollamak isterse
+        elif data[0:3] == "MSG":
+
+            # ":" karakterini koymayi unutmus ise
+            if not ":" in data:
+                response = "ERR"
+                return [response, 0]
+
+            # to_nickname'i ve mesaji ayikla
+            indexRouter = data.index(":")
+            to_nickname = data[4:indexRouter]
+            message = data[indexRouter+1:]
+
+            # Eger kisi listede yoksa
+            lock1.acquire()
+            if not to_nickname in self.fihrist.keys():
+                response = "MNO " + to_nickname
+                lock1.release()
+                return [response, 0]
+
+            # Kisi mevcutsa mesaji kisinin kuyruguna ekle
+            else:
+                queue_message = (to_nickname, self.nickname, message)
+                # gonderilecek threadQueueyu fihristten alip icine yaz
+                self.fihrist[to_nickname].put(queue_message)
+                response = "MOK"
+                lock1.release()
+                return [response, 0]
+
+        # bir seye uymadiysa protokol hatasi verilecek
+        else:
+            response = "ERR"
+            return [response, 0]
+
+    def run(self):
+        self.logQueue.put("Starting " + self.threadName)
+        while True:
+
+            incomingData = self.clientSocket.recv(1024)
+
+            # Istemciye cevap hazirlaniyor.
+            reaction = self.Parser(incomingData)
+            # Istemciye cevap yollaniyor.
+            self.clientSocket.send(reaction[0])
+
+            # Istemci cikmak isterse soket kapaniyor.
+            if reaction[1] == 1:
+                self.clientSocket.close()
+                break
+        # log gonder
+        self.logQueue.put(self.nickname + " has left.")
+        self.logQueue.put("Exiting " + self.threadName)
+
+class writeThread (threading.Thread):
+
+    def __init__(self, threadName, nickname, threadID, clientSocket, clientAddr, fihrist, threadQueue, logQueue):
+        threading.Thread.__init__(self)
+        self.threadName = "read" + threadName
         self.nickname = nickname
         self.threadID = threadID
         self.clientSocket = clientSocket
@@ -21,124 +170,21 @@ class readThread (threading.Thread):
 
     def run(self):
         self.logQueue.put("Starting " + self.threadName)
+
         while True:
+            if not threadQueue.empty():
+                message = threadQueue.get()
+                if len(message) == 1:
+                    message = "SYS " + message
 
-            incomingData = self.clientSocket.recv(1024)
+                elif len(message) == 2:
+                    message = "SAY " + message[0] + " " + message[1]
 
-            # Istemciye cevap hazirlaniyor.
-            reaction = Parser(self, incomingData)
-            # Istemciye cevap yollaniyor.
-            self.clientSocket.send(reaction[0])
-            # Istemci cikmak isterse soket kapaniyor.
-            if reaction[1] == 1:
-                self.clientSocket.close()
-                break
-        # log gonder
-        self.logQueue.put(self.nickname + " has left.")
-        self.logQueue.put("Exiting " + self.threadName)
+                elif len(message) == 3:
+                    message = "MSG " + message[0] + ":" + message[1] + " " + message[2]
 
+                self.clientSocket.send(message)
 
-def Parser(self, data):
-    data = data.strip()
-
-    # henuz giris yapmamis ise ve giris yapmadan islem yapmak isterse "ERL" ve 1 (hatali) dondur
-    if not self.nickname and not data[0:3] == "USR":
-        response = "ERL"
-        return [response, 0]
-
-    # Daha onceden login olmus ise ve login halinde tekrar login olmak istiyorsa "ERL" ve 1 (hatali) dondur
-    elif self.nickname and data[0:3] == "USR":
-        response = "ERL"
-        return [response, 0]
-
-    # Kullanici giris yapmak istiyorsa
-    elif data[0:3] == "USR":
-        self.nickname = data[4:]
-        # Nickname kullanima uygunsa
-        lock1.acquire()
-        if not self.fihrist.has_key(self.nickname):
-            response = "HEL " + self.nickname
-            self.fihrist[self.nickname] = self.threadQueue
-            lock2.acquire()
-            self.logQueue.put(self.nickname + " has joined.")
-            lock2.release()
-            lock1.release()
-            return [response, 0]
-
-        # Nickname kullanima uygun degilse
-        else:
-            # kullanici reddedilecek
-            response = "REJ " + self.nickname
-            lock1.release()
-            return [response, 0]
-
-    # Kullanici cikis yapmak istiyorsa
-    elif data[0:3] == "QUI":
-        response = "BYE " + self.nickname
-        # fihristten sil
-        lock1.acquire()
-        del fihrist [self.nickname]
-        lock1.release()
-        # baglantiyi sil
-        return [response, 1]
-
-    # Kullanici diger kullanicilari listelemek istiyorsa
-    elif data[0:3] == "LSQ":
-        response = "LSA "
-        lock1.acquire()
-        for i in fihrist.keys():
-            response += i + ": "
-        lock1.release()
-        response = response.strip(": ")
-        return [response, 0]
-
-    # Baglanti kontrol komutu
-    elif data[0:3] == "TIC":
-        response = "TOC"
-        return [response, 0]
-
-    # Kullanici genel mesaj yollamak isterse
-    elif data[0:3] == "SAY":
-        queue_message = (self.nickname, data[4:])
-        lock1.acquire()
-        for i in fihrist.keys():
-            self.fihrist[i].put(queue_message)
-        lock1.release()
-        response = "SOK"
-        return [response, 0]
-
-    # Kullanici ozel mesaj yollamak isterse
-    elif data[0:3] == "MSG":
-
-        # to_nickname'i ve mesaji ayikla
-        indexRouter = data.index(":")
-        if indexRouter < 0:
-            response = "ERR"
-            return [response, 0]
-        else:
-            to_nickname = data[4:indexRouter]
-            message = data[indexRouter:]
-
-        # Eger kisi listede yoksa
-        lock1.acquire()
-        if not to_nickname in self.fihrist.keys():
-            response = "MNO " + to_nickname
-            lock1.release()
-            return [response, 0]
-
-        # Kisi mevcutsa mesaji kisinin kuyruguna ekle
-        else:
-            queue_message = (to_nickname, self.nickname, message)
-            # gonderilecek threadQueueyu fihristten alip icine yaz
-            self.fihrist[to_nickname].put(queue_message)
-            response = "MOK"
-            lock1.release()
-            return [response, 0]
-
-    else:
-        # bir seye uymadiysa protokol hatasi verilecek
-        response = "ERR"
-        return [response, 0]
 
 class loggerThread (threading.Thread):
     def __init__(self, threadName, logQueue, logFileName):
@@ -158,7 +204,7 @@ class loggerThread (threading.Thread):
     def run(self):
         self.log("Starting " + self.threadName)
         while True:
-            sleep(10000)
+            time.sleep(10000)
             if not logQueue.empty():
                 lock2.acquire()
                 to_be_logged =  logQueue.get()
@@ -170,7 +216,7 @@ class loggerThread (threading.Thread):
 
 serverSocket = socket.socket()
 host = socket.gethostname()
-port = 12346
+port = 12345
 serverSocket.bind((host, port))
 serverSocket.listen(5)
 
@@ -178,7 +224,6 @@ threadID = 0
 threads = []
 
 fihrist = {}
-threadQueue = Queue.Queue()
 logQueue = Queue.Queue()
 
 lThread = loggerThread("Logger Thread", logQueue, "LogFile")
@@ -196,12 +241,13 @@ while True:
     logQueue.put(connectionString)
     threadID += 1
     threadName = "Thread-" + repr(threadID)
+    threadQueue = Queue.Queue()
     rThread = readThread(threadName ,None ,threadID, clientSocket, clientAddr, fihrist, threadQueue, logQueue)
-#    wThread = writeThread(threadID, clientSocket, addr)
+    wThread = writeThread(threadName ,None ,threadID, clientSocket, clientAddr, fihrist, threadQueue, logQueue)
     threads.append(rThread)
-#    threads.append(wThread)
+    threads.append(wThread)
     rThread.start()
- #   wThread.start()
+    wThread.start()
 
 for i in threads:
     i.join()
